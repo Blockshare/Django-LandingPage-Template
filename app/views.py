ac41d591b43a4da4385640ca830e87b1f3f26dd1
cponeill <caseyoneill78@hotmail.com>
2016-06-26 14:26:20 -0400

    changes

--- app/views.py
+++ app/views.py
@@ -15,7 +15,6 @@ def address(request):
         json_list.append(response.json())
         parsed_data = []
         btc_data = {}
-        price = get_price()
         for data in json_list:
             if data['final_n_tx'] != 0 or not data:
                 btc_data['address'] = data['address']
@@ -24,7 +23,6 @@ def address(request):
                 btc_data['total_received'] = data['total_received'] * 0.00000001
                 btc_data['block_height'] = data['txrefs'][0]['block_height']
                 btc_data['confirmations'] = data['txrefs'][0]['confirmations']
-                btc_data['price'] = price
             else:
                 btc_data['address'] = data['address']
                 btc_data['final_balance'] = data['final_balance'] * 0.00000001
@@ -32,6 +30,5 @@ def address(request):
                 btc_data['total_received'] = data['total_received'] * 0.00000001
                 btc_data['block_height'] = "Unavailable"
                 btc_data['confirmations'] = "Unavailable"
-                btc_data['price'] = price
         parsed_data.append(btc_data)
     return render(request, 'app/profile.html', {'data': parsed_data})
